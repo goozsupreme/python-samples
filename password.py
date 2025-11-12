8689b25453faf145cad78f36d715dec0c01ad6dd
gooz <72416887+goozsupreme@users.noreply.github.com>
2025-11-11 22:19:19 -0500

    password.py
    password

---
+++ password.py
@@ -0,0 +1,16 @@
+upper = 0
+lower = 0
+digit = 0
+
+password = input("Enter password ")
+
+for x in password:
+     for i in range(len(password)>=8):
+          if(password.isupper()):
+                    upper = 1
+          elif(password.islower()):
+                    lower = 1
+          elif(password.isdigit()):
+                    digit = 1
+          else:
+               print("Please try again")
