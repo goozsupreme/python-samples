89d8a6f0d43706057480964ae18b84f33b9da90e
gooz <72416887+goozsupreme@users.noreply.github.com>
2025-11-11 21:44:17 -0500

    rainfall-average.py
    calculate rainfall average

---
+++ rainfall-average.py
@@ -0,0 +1,19 @@
+import math
+
+my_list =["January","Febuary","March","April","May","June","July","August","September","October","November","December"]
+total = 0
+
+total = int(input("Enter number of months it rained ")) #User enters in how many months that it rained
+
+for n in range(0,total):
+     print(my_list[n])
+     amount = int(input("Enter rainfall ")) #User enters in mow much rainfall
+     total += amount #Concatenate the total months they entered and the total amount of rainfall they entered
+
+print("Total is",total) #Prints total amount of months or rainfall?
+
+print(min(amount))
+print(max(amount))
+
+average = total (amount)
+print("The average is",average)
