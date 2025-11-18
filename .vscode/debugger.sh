201ac3f9eed35cc1e545150baba0620fde409fab
Katherine Guzman <72416887+goozsupreme@users.noreply.github.com>
2025-11-18 15:59:44 -0500

    adding shell attribute

--- .vscode/debugger.sh
+++ .vscode/debugger.sh
@@ -1,2 +1,3 @@
-#/bin/bash
+#!/bin/sh
 python -m debugpy --listen 5678 ./{filepath of python program} #example: ./C:\Users\username\Downloads\python-samples\password\password-check.py
+debugger.sh linguist-language=Shell
